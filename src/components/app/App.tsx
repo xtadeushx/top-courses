9f1f56df47594b4b1b7e651b2d5cdcdc94da3fa4
xtadeushx <alekscherkasov13@gmail.com>
2023-03-15 16:29:15 +0200

    feat: add not  found page

--- src/components/app/App.tsx
+++ src/components/app/App.tsx
@@ -5,9 +5,14 @@ import { PaginatedCourses } from 'components/paginate/pagination';
 
 import { Course, } from 'common/types/coursesList.types';
 
-import previewCourses from '../../moch/cursess.json';
+import previewCourses from '../../mock/courses.json';
 
 import styles from './app.module.scss';
+import { Route, Routes } from 'react-router-dom';
+import { AppRoute } from 'common/enums/enums';
+import { NotFoundPage } from 'components/pages/not-found-page/not-found-page';
+import { Registration } from 'components/pages/registration/registration';
+import { Login } from 'components/pages/login/login';
 
 const App = () => {
 
@@ -23,8 +28,23 @@ const App = () => {
         logOut={() => console.log('log out')}
         user='Dijon Dou'
       />
-      <PaginatedCourses courses={courses}
-        loading='succeeded' itemsPerPage={10} />
+      <Routes>
+        <Route path={AppRoute.ROOT}
+          element={<PaginatedCourses courses={courses}
+            loading='succeeded' itemsPerPage={10} />} />
+        <Route path={AppRoute.ANY}
+          element={<NotFoundPage />} />
+        <Route
+          path={AppRoute.LOGIN}
+          element={<Login />}
+        />
+        <Route
+          path={AppRoute.REGISTRATION}
+          element={<Registration />}
+        />
+      </Routes>
+
+
       {/* <CoursesLayout
         courses={courses}
         loading='succeeded'

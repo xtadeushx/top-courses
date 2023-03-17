cf3918167589ec77120fae156edcecaab6863ecc
xtadeushx <alekscherkasov13@gmail.com>
2023-03-17 21:55:42 +0200

    feat: add speed down and speed up

--- src/components/common/video/video.tsx
+++ src/components/common/video/video.tsx
@@ -1,4 +1,4 @@
-import { useState, useRef } from 'hooks/hooks';
+import { useState, useRef, useEffect } from 'hooks/hooks';
 import ReactHlsPlayer from '@gumlet/react-hls-player';
 import styles from './video.module.scss';
 interface IPlayerProps {
@@ -17,10 +17,12 @@ const Player: React.FC<IPlayerProps> = ({
   autoPlay,
   controls,
   muted,
+
 }) => {
   const [isPlaying, setIsPlaying] = useState(false);
   const [progress, setProgress] = useState(duration);
   const videoRef = useRef<HTMLVideoElement>(null);
+  const [playbackRate, setPlaybackRate] = useState(1);
 
   const handleMouseEnter = () => {
     if (videoRef.current) {
@@ -35,6 +37,23 @@ const Player: React.FC<IPlayerProps> = ({
     }
   };
 
+  useEffect(() => {
+    if (videoRef.current !== null) {
+      videoRef.current.playbackRate = playbackRate;
+    }
+  }, [playbackRate]);
+
+  const handelSpeed = (e: any) => {
+    if (e.code === 'KeyQ' && playbackRate <= 0.25) {
+      setPlaybackRate(prev => prev + 0.25)
+      console.log(`speed was increased to ${playbackRate}`)
+    }
+    if (e.code === 'KeyW' && playbackRate >= 0.5) {
+      setPlaybackRate(prev => prev - 0.25)
+      console.log(`speed was decrees to ${playbackRate}`)
+    }
+  }
+
   const handleProgress = () => {
     if (videoRef.current !== null) {
       const duration = videoRef.current.duration;
@@ -44,19 +63,28 @@ const Player: React.FC<IPlayerProps> = ({
     }
   };
   return (
-    <ReactHlsPlayer
-      src={link}
-      autoPlay={autoPlay}
-      controls={controls}
-      width="100%"
-      height="100%"
-      playerRef={videoRef}
-      poster={poster + '/cover.webp'}
-      onMouseEnter={handleMouseEnter}
-      onMouseLeave={handleMouseLeave}
-      muted={muted}
-      className={styles.video}
-    />
+    <>
+      <div className={styles['video-wrapper']}>
+        <ReactHlsPlayer
+          src={link}
+          autoPlay={autoPlay}
+          controls={controls}
+          width="100%"
+          height="100%"
+          playerRef={videoRef}
+          poster={poster + '/cover.webp'}
+          // onMouseEnter={handleMouseEnter}
+          // onMouseLeave={handleMouseLeave}
+          muted={muted}
+          className={styles['video']}
+          onKeyDown={handelSpeed}
+
+        />
+      </div>
+
+      <span className={styles['tooltip-text__up']}>to up speed press 'W' </span>
+      <span className={styles['tooltip-text__down']}>to down speed press 'Q' </span>
+    </>
   );
 };
 
